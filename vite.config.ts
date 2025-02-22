e502dc41085343562d65ebe575078606742d93e2
taisan11 <131413638+taisan11@users.noreply.github.com>
2025-02-22 15:45:28 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,21 @@ import cloudflare from '@hono/vite-dev-server/cloudflare'
 import { defineConfig } from 'vite'
 import honox from "honox/vite"
 
-export default defineConfig({
-  css:{
-    transformer:"lightningcss"
-  },
-  resolve:{
-    alias:{
-      "@":new URL("app", import.meta.url).pathname
-    }
-  },
-  plugins: [
-    honox({devServer:{adapter:cloudflare()}}),
-    build({})
-  ]
-})
+export default defineConfig(({ command }) => {
+  const isDev = command === 'serve'; // 開発環境なら true, ビルドなら false
+
+  return {
+    css: {
+      transformer: "lightningcss"
+    },
+    resolve: {
+      alias: {
+        "@": new URL("app", import.meta.url).pathname
+      }
+    },
+    plugins: [
+      honox({ devServer: { adapter: isDev ? cloudflare() : undefined } }),
+      build({})
+    ]
+  };
+});
