2a837a9768b5a716ede5146f1515c32ed3060bb6
taisan11 <131413638+taisan11@users.noreply.github.com>
2025-03-13 20:29:38 +0900

    a

--- Deno_proxy.ts
+++ Deno_proxy.ts
@@ -2,20 +2,23 @@ import { Hono } from "hono";
 import {proxy} from "hono/proxy"
 import { cors } from "hono/cors";
 import {etag} from "hono/etag"
+import {logger} from "hono/logger"
 
 const cache = await caches.open("my-cache");
 const app = new Hono();
 
 app.use(cors());
 app.use(etag());
+app.use(logger());
 
 app.all("/proxy/:url{.+}", async (c) => {
     const cacheResponse = await cache.match(c.req.raw);
     if (cacheResponse) {
         return cacheResponse;
     } else {
-        const re = await proxy(c.req.raw.url, c.req.raw);
-        cache.put(c.req.raw, re);
+        const re = await proxy(c.req.url.replace("https://paxiv-proxy-11.deno.dev/proxy/",""), c.req.raw);
+        const cacheRequest = new Request(c.req.url, c.req);
+        cache.put(cacheRequest, re);
         return re;
     }
 })
